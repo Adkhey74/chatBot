02f70685446bba2bb17da9fa5ba544510b574d75
Ultimaterror <ultimaterror@laposte.net>
2025-05-20 11:20:34 +0200

    feat: Get User Vehicles

--- src/Controller/Api/UserVehicleController.php
+++ src/Controller/Api/UserVehicleController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserVehicleController extends AbstractController
 {
-    #[Route('/api/users/{id}/vehicles', name: 'user_vehicles', methods: ['GET'])]
-    public function __invoke(User $user): JsonResponse
+    #[Route('/api/user/vehicles', name: 'user_vehicles', methods: ['GET'])]
+    public function __invoke(Security $security): JsonResponse
     {
+        $user = $security->getUser();
         return $this->json($user->getVehicles(), 200, [], ['groups' => ['vehicle:read']]);
     }
 }
